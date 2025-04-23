ec67546a359e5767194cd6a07206fdb8974adc79
Kane Kabena <onekaneldn@gmail.com>
2025-04-23 16:52:53 +0300

    updated App.jsx / AppRouter.jsx to impliment UseFetchBooks custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import NavBar from "./components/Navbar"
-import AppRouter from "./router/AppRouter"
+import React, { useState } from 'react';
+import NavBar from "./components/Navbar";
+import AppRouter from "./router/AppRouter";
 import { BookProvider } from "./context/BookContext";
+import useFetchBooks from "./hooks/UseFetch"; 
 
 function App() {
+  const [query, setQuery] = useState("");  // Query state for searching books
+  const { books, isLoading, error } = useFetchBooks(query);  // Using the hook to fetch books based on the query
+
   return (
-   <BookProvider>
-    <NavBar />
-    <main>
-      <AppRouter />
-    </main>
-   </BookProvider>
+    <BookProvider>
+      <NavBar />
+      <main>
+        {/* You can pass the fetched books and the query setter to the AppRouter */}
+        <AppRouter 
+          query={query} 
+          setQuery={setQuery} 
+          books={books} 
+          isLoading={isLoading} 
+          error={error} 
+        />
+      </main>
+    </BookProvider>
   );
 }
 
