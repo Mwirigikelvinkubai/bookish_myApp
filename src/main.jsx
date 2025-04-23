9db5f001f2e835463b642a3d59e6294666dd6122
mwirigikelvinkubai <mwirigiomukombiti@gmail.com>
2025-04-23 12:07:21 +0300

    structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import BookList from './components/BookList.jsx';
+import BookModal from './components/BookModal.jsx';
+import BookSearch from './components/BookSearch.jsx';
+import GenreFilter from './components/GenreFilter.jsx';
+import WishlistButton from './components/WishlistButton.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<BookList />} />
+        <Route path="search" element={<BookSearch />} />
+        <Route path="filter" element={<GenreFilter />} />
+        <Route path="wishlist" element={<WishlistButton />} />
+        <Route path="book/:id" element={<BookModal />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
