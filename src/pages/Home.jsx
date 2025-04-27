7021a36d555241f8cee4fdd4244b765397cc1cf1
shirocodes <ishirapt@gmail.com>
2025-04-27 08:15:27 +0300

    Improvedcontext, custom hooks, and search functionalities.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,15 @@
+import React from "react";
 import { useBookContext } from "../context/BookContext";
 import BookGrid from "../components/BookGrid";
 import BookCard from "../components/BookCard";
+import Search from "../components/Search";
 
 const Home = () => {
   const { books, query, setQuery, isLoading, error } = useBookContext()
 
   return (
     <div className="p-6">
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search for books..."
-          className="px-4 py-2 rounded-lg w-full max-w-md text-black"
-        />
-      </div>
+      <Search query={query} setQuery={setQuery} />
 
       {isLoading && <p className="text-center text-gray-400">Loading...</p>}
       {error && <p className="text-center text-red-500">{error}</p>}
