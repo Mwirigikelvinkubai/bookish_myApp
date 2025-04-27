c54451eab5c32e4732a1e41ab89bbf2babce4e4b
shirocodes <ishirapt@gmail.com>
2025-04-27 22:07:00 +0300

    Improved Login with validation, UI, and filtered fetch via getUser func.

--- src/api/userAPI/getUsers.js
+++ src/api/userAPI/getUsers.js
@@ -1,8 +1,12 @@
-const getUsers = async () => {
-    const res = await fetch('http://localhost:3001/users');
-    if (!res.ok) throw new Error('Failed to fetch users');
-    return await res.json();
-  };
-  
-  export default getUsers;
+const getUsers = async (username) => {
+
+  //filter to get user by username
+  const res = await fetch(`http://localhost:3001/users?username=${username}`);
+  if (!res.ok) throw new Error('Failed to fetch user');
+  const users = await res.json();
+  return users[0]; // Return the first user since username is unique to one user
+};
+
+export default getUsers;
+
   
