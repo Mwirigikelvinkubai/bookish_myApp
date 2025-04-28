73231150ad5f9215bf78c98622abb320f07b4992
shirocodes <ishirapt@gmail.com>
2025-04-28 17:07:30 +0300

    Fetchuser with mock api update.

--- src/api/userAPI/fetchUser.js
+++ src/api/userAPI/fetchUser.js
@@ -2,7 +2,7 @@ import getCurrentUserId from "./getCurrentUserId";
 
 const fetchUser = async () => {
   const userId = getCurrentUserId();
-  const response = await fetch(`http://localhost:3001/users/${userId}`);
+  const response = await fetch(`https://680f048c67c5abddd193916e.mockapi.io/bookishV1/users/${userId}`);
   if (!response.ok) throw new Error("Failed to fetch user data");
   return response.json();
 };
