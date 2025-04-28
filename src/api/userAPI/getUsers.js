61142574895198c11eba51742584176f98171c4e
shirocodes <ishirapt@gmail.com>
2025-04-28 16:59:32 +0300

    Refactored code with Mock API.

--- src/api/userAPI/getUsers.js
+++ src/api/userAPI/getUsers.js
@@ -1,7 +1,7 @@
 const getUsers = async (username) => {
 
   //filter to get user by username
-  const res = await fetch(`http://localhost:3001/users?username=${username}`);
+  const res = await fetch(`https://680f048c67c5abddd193916e.mockapi.io/bookishV1/users?username=${username}`);
   if (!res.ok) throw new Error('Failed to fetch user');
   const users = await res.json();
   return users[0]; // Return the first user since username is unique to one user
